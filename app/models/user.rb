147d4d2b9725c7c9647352dd8ffa1d504190e6d0
Fox <coriusfoxus@gmail.com>
2016-02-24 23:12:04 -0500

    Concluded 10.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,9 +54,7 @@ class User < ActiveRecord::Base
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest, User.digest(reset_token))
-    puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
     puts Time.zone.now
-    puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
     update_attribute(:reset_sent_at, Time.zone.now)
   end
 
