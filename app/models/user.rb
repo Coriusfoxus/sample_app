139bd74b5de4defa7f34fce1ee8822d6683d1258
Fox <coriusfoxus@gmail.com>
2016-02-24 23:39:53 -0500

    Finish chapter 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,8 +41,7 @@ class User < ActiveRecord::Base
 
   #Activate an account
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # Send activation email
@@ -53,9 +52,7 @@ class User < ActiveRecord::Base
   # Set the password reset attributes
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    puts Time.zone.now
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now) 
   end
 
   def send_password_reset_email
